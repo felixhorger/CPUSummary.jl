84e0aaa632fa5557123418a5b176f46b99d6466d
Felix Horger <felix.horger@kcl.ac.uk>
2023-01-06 17:31:29 +0100

    Removed obsolete @show, adjusted tabs

--- src/CPUSummary.jl
+++ src/CPUSummary.jl
@@ -1,3 +1,4 @@
+
 module CPUSummary
 
 	using Static
@@ -19,21 +20,22 @@ module CPUSummary
 	cache_linesize() = cache_linesize(Val(1))
 
 	function num_cache_levels()
-	  numl4 = num_l4cache()
-	  numl4 === nothing && return nothing
-	  ifelse(
-		eq(numl4, Zero()),
+		numl4 = num_l4cache()
+		numl4 === nothing && return nothing
 		ifelse(
-		  eq(num_l3cache(), Zero()),
-		  ifelse(
-			eq(num_l2cache(), Zero()),
-			ifelse(eq(num_l1cache(), Zero()), Zero(), One()),
-			StaticInt{2}(),
-		  ),
-		  StaticInt{3}(),
-		),
-		StaticInt{4}(),
-	  )
+			eq(numl4, Zero()),
+			ifelse(
+				eq(num_l3cache(), Zero()),
+				ifelse(
+					eq(num_l2cache(), Zero()),
+					ifelse(eq(num_l1cache(), Zero()), Zero(), One()),
+					StaticInt{2}(),
+				),
+				StaticInt{3}(),
+			),
+			StaticInt{4}(),
+		)
 	end
 
 end
+
